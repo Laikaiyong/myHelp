24068887c74a1b2d16060bbb0c37b561eac3d9ae
Vandyck <vandycklai@gmail.com>
2023-12-07 00:19:09 +0800

    Hide for now

--- frontend/src/app/performance/community/community.component.ts
+++ frontend/src/app/performance/community/community.component.ts
@@ -13,32 +13,28 @@ export class CommunityComponent  implements OnInit {
   ngOnInit(): void {
   }
   
-  lineChart = new Chart({
-    chart: {
-      type: 'line'
-    },
-    title: {
-      text: 'Overall Course Performance'
-    },
-    credits: {
-      enabled: false
-    },
-    series: [
-      {
-        name: 'Total Course Commencement',
-        data: [10, 2, 3, 6, 9, 17, 20, 10, 5, 2, 16]
-      },
-      {
-        name: 'Total Course Completion',
-        data: [5, 8, 12, 15, 20, 25, 18, 14, 10, 6, 3]
-      },
-      {
-        name: 'Total Points',
-        data: [15, 10, 8, 14, 25, 22, 30, 18, 12, 9, 5]
-      }
-    ]
-});
-
-
-
-
+  // lineChart = new Chart({
+  //   chart: {
+  //     type: 'line'
+  //   },
+  //   title: {
+  //     text: 'Overall Course Performance'
+  //   },
+  //   credits: {
+  //     enabled: false
+  //   },
+  //   series: [
+  //     {
+  //       name: 'Total Course Commencement',
+  //       data: [10, 2, 3, 6, 9, 17, 20, 10, 5, 2, 16]
+  //     },
+  //     {
+  //       name: 'Total Course Completion',
+  //       data: [5, 8, 12, 15, 20, 25, 18, 14, 10, 6, 3]
+  //     },
+  //     {
+  //       name: 'Total Points',
+  //       data: [15, 10, 8, 14, 25, 22, 30, 18, 12, 9, 5]
+  //     }
+  //   ]
+}
