d5d411903a394172a008c84c8891ac01b9abd795
Vandyck <vandycklai@gmail.com>
2024-01-06 10:50:03 +0800

    Add Deployed Code

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,3 +1,18 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyA28ZLTGqegNI1H8FxpDzzq6n0zBtQVnLQ",
+    authDomain: "flutter-frenzy.firebaseapp.com",
+    databaseURL: "https://flutter-frenzy-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "flutter-frenzy",
+    locationId:"asia-southeast1",
+    storageBucket: "flutter-frenzy.appspot.com",
+    messagingSenderId: "90982953877",
+    appId: "1:90982953877:web:114860f8e44935f7837c9c",
+    measurementId: "G-BCHMQHT4C1"
+  },
+
+  palmApi: {
+    apiKey: "AIzaSyAEJ5xUSlq0bw-0z5awQD2_7wrH21XQ5-0"
+  }
 };
